ee6864876b39da9a449c1499f557ed297e5207ec
jorsae <jorgen.sater@gmail.com>
2020-08-20 06:31:24 +1000

    !next will now display time left and if an event is currently ongoing

--- event.py
+++ event.py
@@ -1,24 +1,37 @@
 import math
-from datetime import datetime
+from datetime import datetime, timedelta
 import constants
 
 def next(ctx, settings, iterations):
     iterations = clean_iterations(iterations)
-    print(f'iterations: {iterations}')
 
     if is_event(settings) is False:
-        # TODO: Get the closest event
-        return 'No event is currently running'
+        next_event = get_next_event(settings)
+        if next_event is None:
+            return 'No event is currently running.'
+        time_left = next_event - datetime.utcnow()
+        return f'No event is currently running.\nNext event in {time_left}'
     
     difference = (datetime.utcnow() - settings.start_time).total_seconds()
-    print(f'difference: {difference}')
-    event_iterations = math.floor(difference / 3600)
+    event_iterations = math.floor(difference / constants.ONE_HOUR)
     next_event = (settings.start_event + event_iterations) % 3
 
-    output = ''
+    time_difference = constants.SIX_HOURS - (difference % constants.SIX_HOURS)
+    
+    output = f'Next {iterations} event(s)\n'
+
+    # Event is currently ongoing NOW
+    if (constants.SIX_HOURS - time_difference) < 1800:
+        b = constants.SIX_HOURS - time_difference
+        event = (next_event - 1) % 3
+        a = timedelta(seconds=b)
+        output += f'{constants.EVENTS[event]} ongoing. Time left: {a}\n'
+    
     for i in range(iterations):
+        time_left = i*constants.SIX_HOURS + time_difference
+        a = timedelta(seconds=time_left)
         event = (next_event + i) % 3
-        output += f'Next event: {constants.EVENTS[event]}\n'
+        output += f'[{i+1}]: {constants.EVENTS[event]} in {a}\n'
 
     return output
 
