0c2a2b7da832d7f1d4fcaeb8c2517c1ab2260e47
jorsae <jorgen.sater@gmail.com>
2020-08-20 08:19:41 +1000

    Added logging

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import discord
 import asyncio
+import os
+import logging
 from datetime import datetime
 from discord.ext import commands
 from settings import Settings
@@ -15,6 +17,7 @@ async def test(ctx, arg):
 
 @bot.command(name='next')
 async def next(ctx, iterations: str="1"):
+    logging.info(f'next executed by: {ctx.author}, arg: {iterations}')
     await ctx.send(event.next(ctx, settings, iterations))
 
 @bot.event
@@ -30,30 +33,41 @@ async def on_message(message: discord.Message):
     await bot.process_commands(message)
 
 async def ping_reminder():
-    print('ping reminder is running')
+    logging.info(f'Ping reminder is running: {settings.run_ping_reminder}')
     while settings.run_ping_reminder:
         difference = (datetime.utcnow() - settings.start_time).total_seconds()
         time_difference = constants.SIX_HOURS - (difference % constants.SIX_HOURS)
         if time_difference <= constants.WARNING_TIME:
-            if is_event(settings):
+            if event.is_event(settings):
                 event_iterations = math.floor(difference / constants.ONE_HOUR)
                 next_event = (settings.start_event + event_iterations) % 3
-                print('ping reminder')
+                logging.info(f'Ping reminder: {constants.EVENTS[event]} | {event_iterations}, {next_event}')
             else:
+                logging.debug(f'No event is ongoing')
                 asyncio.sleep(constants.WARNING_TIME)
         else:
-            print(f'time_difference: {time_difference}')
             sleep_time = time_difference - constants.WARNING_TIME
+            logging.info(f'Sleep time: {sleep_time}')
             await asyncio.sleep(sleep_time)
 
 async def do_tasks():
     await bot.wait_until_ready()
-    print(f"Logged in as {bot.user}")
+    print(f'Logged in as {bot.user}')
+    logging.info(f'Logged in as {bot.user}')
     if settings.start_ping_reminder:
         settings.start_ping_reminder = False
         bot.loop.create_task(ping_reminder())
 
+def setup_logging():
+    logFolder = 'logs'
+    logFile = 'CatGameBot.log'
+    if not os.path.isdir(logFolder):
+        os.makedirs(logFolder)
+    
+    logging.basicConfig(filename=f'{logFolder}/{logFile}', level=logging.INFO, format='%(asctime)s %(levelname)s:[%(filename)s:%(lineno)d] %(message)s')
+        
 if __name__ == '__main__':
+    setup_logging()
     settings.parse_settings()
     # print(settings)
     # print(event.get_next_event(settings))
